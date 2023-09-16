0c9642f7ca8c67f71d480d7b05b7b3645ee4fb33
himanshu21072002 <himanshu.gangwar2000@gmail.com>
2023-09-16 16:42:13 +0530

    update functionality added

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,10 @@
 import Express  from "express";
-import {getPosts, createPost} from "../controller/post.js";
+import {getPosts, createPost, updatePost} from "../controller/post.js";
 
 const router= Express.Router();
 
 router.get("/",getPosts);
 router.post("/",createPost);
+router.patch('/:id',updatePost);
 
 export default router;
