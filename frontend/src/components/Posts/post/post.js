12b82b2114d73ccd098b333f58855283716acde7
himanshu21072002 <himanshu.gangwar2000@gmail.com>
2023-09-16 11:49:54 +0530

    material icon and card implemented

--- frontend/src/components/Posts/post/post.js
+++ frontend/src/components/Posts/post/post.js
@@ -1,11 +1,47 @@
 import React from "react";
+import { Card,CardActions, CardContent,CardMedia,Button, Typography } from "@mui/material";
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import moment from "moment";
+
 import "./style.css";
-function Post(){
+function Post({post}){
 
     return (
-        <h1>
-            POST
-        </h1>
+        <Card className="card">
+            <CardMedia className="media" image={post.selectedFile} title={post.title}></CardMedia>
+            <div className="overlay">
+                <Typography variant="h6">
+                {post.creator}
+                </Typography>
+                <Typography variant="body2">
+                {moment(post.createdAt).fromNow()}
+                </Typography>
+            </div>
+            <div className="overlay2">
+                <Button style={{color: "white"}} size="small" onClick={()=>{}}>
+                    <MoreHorizIcon fontSize="default"/>
+                </Button>
+            </div>
+            <div className="details">
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tag)=>`#${tag} `)}</Typography>
+            </div>
+            <CardContent>
+            <Typography className="title" variant="h5" gutterBottom>{post.message}</Typography>
+            </CardContent>
+            <CardActions className="cardActions">
+                <Button size="small" color="primary" onClick={()=>{}}>
+                    <ThumbUpAltIcon fontSize="small"/>
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={()=>{}}>
+                    <DeleteIcon fontSize="small"/>
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
     );
 }
 export default Post;
--- frontend/src/components/Posts/posts.js
+++ frontend/src/components/Posts/posts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Post from "./post/post";
+import { Grid, CircularProgress } from "@mui/material";
 import "./style.css";
 import { useSelector } from "react-redux"; 
 
@@ -7,11 +8,19 @@ function Posts() {
   const posts= useSelector((state)=>state.posts);
   console.log(posts);
   return (
-    <div>
-      <h1 >POSTS</h1>
-      <Post/>
-      <Post/>
-    </div>
+    !posts.length ? <CircularProgress /> : 
+      (
+        <Grid className="mainContainer" container alignItems="stretch" spacing={3} > 
+          {
+            posts.map((post)=>(
+              <Grid key={post._id} item xs={12} sm={6}>
+                  <Post post={post}>
+                  </Post>
+                </Grid>
+            ))
+          }
+        </Grid>
+      )
   );
 }
 export default Posts;
